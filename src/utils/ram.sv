51e86bf98d92dfd43103756eefdb1aa8b9e55ef0
xinhecuican <lizilin2686462757@gmail.com>
2024-09-13 14:56:23 +0800

    chore: sram add bank config

--- src/utils/ram.sv
+++ src/utils/ram.sv
@@ -292,8 +292,8 @@ generate;
     else if(READ_LATENCY == 1)begin
         if(RESET)begin
             for(genvar i=0; i<READ_PORT; i++)begin
-                always_ff @(posedge clk or posedge rst)begin
-                    if(rst == `RST)begin
+                always_ff @(posedge clk)begin
+                    if(!ready)begin
                         rdata[i] <= 0;
                     end
                     else if(en[i]) begin
@@ -302,8 +302,8 @@ generate;
                 end
             end
             for(genvar i=0; i<RW_PORT; i++)begin
-                always_ff @(posedge clk or posedge rst)begin
-                    if(rst == `RST)begin
+                always_ff @(posedge clk)begin
+                    if(!ready)begin
                         rdata[READ_PORT+i] <= 0;
                     end
                     else if(en[READ_PORT+i]) begin
@@ -346,7 +346,7 @@ endgenerate
 
 endmodule
 
-module MPRAM #(
+module MPRAMInner #(
     parameter WIDTH = 32,
     parameter DEPTH = 8,
     parameter READ_PORT = 4,
@@ -368,7 +368,6 @@ module MPRAM #(
     output logic `ARRAY(READ_PORT+RW_PORT, WIDTH) rdata,
     output logic ready
 );
-
     MPREG #(
         .WIDTH(WIDTH),
         .DEPTH(DEPTH),
@@ -379,6 +378,189 @@ module MPRAM #(
         .BYTE_WRITE(BYTE_WRITE),
         .RESET(RESET)
     ) regs (.*);
+
+    // initial begin
+    //     $display("%0dr%0dw%0drw_%0dx%0d%s", READ_PORT, WRITE_PORT, RW_PORT, WIDTH, DEPTH, BYTE_WRITE ? "_8" : "");
+    // end
+endmodule
+
+module MPRAM #(
+    parameter WIDTH = 32,
+    parameter DEPTH = 8,
+    parameter READ_PORT = 4,
+    parameter WRITE_PORT = 4,
+    parameter RW_PORT = 0,
+    parameter BANK_SIZE = DEPTH,
+    parameter ADDR_WIDTH = $clog2(DEPTH),
+    parameter READ_LATENCY = 1,
+    parameter BYTE_WRITE = 0,
+    parameter RESET = 0,
+    parameter BYTES = BYTE_WRITE == 1 ? WIDTH / 8 : 1
+)(
+    input logic clk,
+    input logic rst,
+    input logic `N(READ_PORT+RW_PORT) en,
+    input logic `ARRAY(READ_PORT, ADDR_WIDTH) raddr,
+    input logic `ARRAY(WRITE_PORT+RW_PORT, ADDR_WIDTH) waddr,
+    input logic [WRITE_PORT+RW_PORT-1: 0][BYTES-1: 0] we,
+    input logic [WRITE_PORT+RW_PORT-1: 0][BYTES-1: 0][WIDTH/BYTES-1: 0] wdata,
+    output logic `ARRAY(READ_PORT+RW_PORT, WIDTH) rdata,
+    output logic ready
+);
+generate
+    if(DEPTH > BANK_SIZE)begin
+        localparam BANK = DEPTH / BANK_SIZE;
+        localparam REMAIN = DEPTH % BANK_SIZE;
+        localparam BANK_ALL = REMAIN != 0 ? BANK + 1 : BANK;
+        logic `ARRAY(READ_PORT, BANK_ALL) bank_raddr_en;
+        logic `ARRAY(WRITE_PORT+RW_PORT, BANK_ALL) bank_waddr_en;
+        logic `ARRAY(BANK_ALL, READ_PORT+RW_PORT) bank_en;
+        logic `ARRAY(BANK_ALL, WRITE_PORT+RW_PORT) bank_we;
+        logic `ARRAY(READ_PORT, ADDR_WIDTH-$clog2(BANK_ALL)) raddr_bank;
+        logic `ARRAY(WRITE_PORT+RW_PORT, ADDR_WIDTH-$clog2(BANK_ALL)) waddr_bank;
+        logic `TENSOR(BANK_ALL, READ_PORT+RW_PORT, BYTES) we_bank;
+        logic `TENSOR(BANK_ALL, READ_PORT+RW_PORT, WIDTH) rdata_bank;
+        logic `N(BANK) ready_bank;
+        logic `ARRAY(READ_PORT+RW_PORT, $clog2(BANK_ALL)) bank_idx, ridx;
+        logic `ARRAY(WRITE_PORT+RW_PORT, BYTES*WIDTH) wdata_bank;
+
+        logic `N(ADDR_WIDTH+1) counter;
+        logic resetState;
+
+        if(RESET)begin
+            always_ff @(posedge clk or posedge rst)begin
+                if(rst == `RST)begin
+                    counter <= 0;
+                    resetState <= 1'b1;
+                    ready <= 1'b0;
+                end
+                else begin
+                    if(resetState)begin
+                        counter <= counter + 1;
+                        if(counter == BANK_SIZE)begin
+                            resetState <= 1'b0;
+                            ready <= 1'b1;
+                        end
+                    end
+                end
+            end
+        end
+        else begin
+            assign ready = ready_bank[0];
+        end
+
+        for(genvar i=0; i<READ_PORT; i++)begin
+            Decoder #(BANK_ALL) decoder_bank (raddr[i][ADDR_WIDTH-1:ADDR_WIDTH-$clog2(BANK_ALL)], bank_raddr_en[i]);
+            assign raddr_bank[i] = raddr[i][ADDR_WIDTH-$clog2(BANK_ALL)-1: 0];
+            assign bank_idx[i] = raddr[i][ADDR_WIDTH-1: ADDR_WIDTH-$clog2(BANK_ALL)];
+        end
+        for(genvar i=0; i<RW_PORT; i++)begin
+            assign bank_idx[i+READ_PORT] = waddr[WRITE_PORT+i][ADDR_WIDTH-1: ADDR_WIDTH-$clog2(BANK_ALL)];
+        end
+        for(genvar i=0; i<WRITE_PORT+RW_PORT; i++)begin
+            if(RESET && i == 0)begin
+                assign waddr_bank[i] = resetState ? counter : waddr[i][ADDR_WIDTH-$clog2(BANK_ALL)-1: 0];
+                assign wdata_bank[i] = resetState ? 0 : wdata[i];
+            end
+            else begin
+                assign waddr_bank[i] = waddr[i][ADDR_WIDTH-$clog2(BANK_ALL)-1: 0];
+                assign wdata_bank[i] = wdata[i];
+            end
+            Decoder #(BANK_ALL) decoder_bank (waddr[i][ADDR_WIDTH-1:ADDR_WIDTH-$clog2(BANK_ALL)], bank_waddr_en[i]);
+        end
+        for(genvar i=0; i<BANK_ALL; i++)begin
+            for(genvar j=0; j<READ_PORT; j++)begin
+                assign bank_en[i][j] = bank_raddr_en[j][i];
+            end
+            for(genvar j=0; j<RW_PORT; j++)begin
+                assign bank_en[i][j+READ_PORT] = bank_waddr_en[j+WRITE_PORT][i];
+            end
+            for(genvar j=0; j<WRITE_PORT+RW_PORT; j++)begin
+                assign bank_we[i][j] = bank_waddr_en[j][i];
+                for(genvar k=0; k<BYTES; k++)begin
+                    if(RESET && j == 0)begin
+                        assign we_bank[i][j][k] = resetState | we[j][k] & bank_we[i][j];
+                    end
+                    else begin
+                        assign we_bank[i][j][k] = we[j][k] & bank_we[i][j];
+                    end
+                end
+            end
+        end
+
+        if(READ_LATENCY == 0)begin
+            assign ridx = bank_idx;
+        end
+        else if(READ_LATENCY == 1)begin
+            logic `ARRAY(READ_PORT+RW_PORT, $clog2(BANK_ALL)) bank_idx_n;
+            always_ff @(posedge clk)begin
+                bank_idx_n <= bank_idx;
+            end
+            assign ridx = bank_idx_n;
+        end
+        for(genvar i=0; i<READ_PORT+RW_PORT; i++)begin
+            assign rdata[i] = rdata_bank[ridx[i]][i];
+        end
+        for(genvar i=0; i<BANK; i++)begin
+            MPRAMInner #(
+                .WIDTH(WIDTH),
+                .DEPTH(BANK_SIZE),
+                .READ_PORT(READ_PORT),
+                .WRITE_PORT(WRITE_PORT),
+                .RW_PORT(RW_PORT),
+                .READ_LATENCY(READ_LATENCY),
+                .BYTE_WRITE(BYTE_WRITE),
+                .RESET(0)
+            ) ram (
+                .clk,
+                .rst,
+                .en((en & bank_en[i])),
+                .raddr(raddr_bank[i]),
+                .waddr(waddr_bank[i]),
+                .we(we_bank[i]),
+                .wdata(wdata_bank),
+                .rdata(rdata_bank[i]),
+                .ready(ready_bank[i])
+            );
+        end
+        if(REMAIN > 0)begin
+            MPRAMInner #(
+                .WIDTH(WIDTH),
+                .DEPTH(REMAIN),
+                .READ_PORT(READ_PORT),
+                .WRITE_PORT(WRITE_PORT),
+                .RW_PORT(RW_PORT),
+                .READ_LATENCY(READ_LATENCY),
+                .BYTE_WRITE(BYTE_WRITE),
+                .RESET(0)
+            ) ram (
+                .clk,
+                .rst,
+                .en((en & bank_en[BANK])),
+                .raddr(raddr_bank[BANK]),
+                .waddr(waddr_bank[BANK]),
+                .we(we_bank[BANK]),
+                .wdata(wdata_bank),
+                .rdata(rdata_bank[BANK]),
+                .ready()
+            );
+        end
+    end
+    else begin
+        MPRAMInner #(
+            .WIDTH(WIDTH),
+            .DEPTH(DEPTH),
+            .READ_PORT(READ_PORT),
+            .WRITE_PORT(WRITE_PORT),
+            .RW_PORT(RW_PORT),
+            .READ_LATENCY(READ_LATENCY),
+            .BYTE_WRITE(BYTE_WRITE),
+            .RESET(RESET)
+        ) ram (
+            .*
+        );
+    end
+endgenerate
 
 endmodule
 
