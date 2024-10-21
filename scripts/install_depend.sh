a1af1bfb9e2eadd23668f99ab2c833a0873517e3
xinhecuican <lizilin2686462757@gmail.com>
2024-10-21 09:16:21 +0800

    workflow

--- scripts/install_depend.sh
+++ scripts/install_depend.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+install_dependencies() {
+    sudo apt-get install -y gcc-11 g++-11 build-essential cmake tclsh ant default-jre swig google-perftools libgoogle-perftools-dev python3 python3-dev python3-pip uuid uuid-dev tcl-dev flex libfl-dev git pkg-config libreadline-dev bison libffi-dev wget python3-orderedmultidict
+    sudo apt-get install git help2man perl python3 make
+    sudo apt-get install g++  # Alternatively, clang
+    sudo apt-get install libgz
+    sudo apt-get install libfl2
+    sudo apt-get install libfl-dev
+    sudo apt-get install zlibc zlib1g zlib1g-dev
+    sudo apt-get install ccache
+    sudo apt-get install mold
+    sudo apt-get install libgoogle-perftools-dev numactl
+    sudo apt-get install perl-doc
+    sudo apt-get install git autoconf flex bison
+}
+
 install_riscv_gnu_toolchain() {
     git clone --recursive https://github.com/riscv/riscv-gnu-toolchain
     pushd riscv-gnu-toolchain
@@ -26,17 +41,6 @@ install_riscv_isa_sim() {
 }
 
 install_verilator() {
-    sudo apt-get install git help2man perl python3 make
-    sudo apt-get install g++  # Alternatively, clang
-    sudo apt-get install libgz
-    sudo apt-get install libfl2
-    sudo apt-get install libfl-dev
-    sudo apt-get install zlibc zlib1g zlib1g-dev
-    sudo apt-get install ccache
-    sudo apt-get install mold
-    sudo apt-get install libgoogle-perftools-dev numactl
-    sudo apt-get install perl-doc
-    sudo apt-get install git autoconf flex bison
     git clone https://github.com/verilator/verilator
     pushd verilator
     git checkout v5.024-61-gee130cb20
@@ -57,7 +61,6 @@ install_dramsim3() {
 }
 
 install_yosys() {
-    sudo apt install -y gcc-11 g++-11 build-essential cmake tclsh ant default-jre swig google-perftools libgoogle-perftools-dev python3 python3-dev python3-pip uuid uuid-dev tcl-dev flex libfl-dev git pkg-config libreadline-dev bison libffi-dev wget python3-orderedmultidict
     git clone https://github.com/chipsalliance/synlig.git
     pushd synlig
         git submodule sync
@@ -97,3 +100,6 @@ main() {
         shift
     done
 }
+
+install_dependencies
+main
